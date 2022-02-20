e972af80329dac6c1614b23adfde18dff3512d65
Nitin <nitinchatwani97@gmail.com>
2022-02-21 02:03:57 +0530

    Added procfile, .gitignore and updated ports

--- app.js
+++ app.js
@@ -193,6 +193,8 @@ app.post("/delete", (req, res) => {
 // //     workItems.push(item);
 // //     res.redirect("/");
 // // })
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Server is listening to port 3000")
-);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => console.log("Server is listening to port 3000"));
